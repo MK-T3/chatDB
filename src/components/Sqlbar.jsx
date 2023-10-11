84f81139692512d6f4ec1d056c9beb2ccafffa51
changhyeon12333 <ckd5508@naver.com>
2023-10-11 22:31:14 +0900

    BtnLogoChange

--- src/components/Sqlbar.jsx
+++ src/components/Sqlbar.jsx
@@ -1,40 +1,30 @@
 import React, { useState } from "react";
 import styles from "./Sqlbar.module.css";
-
+import {ChevronDownIcon} from "@heroicons/react/24/outline"
 const Sqlbar = ({ children }) => {
-  const [isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setOpen(!isOpen);
-  };
+    const toggleMenu = () => {
+        setOpen(!isOpen);
+    };
 
-  return (
-    <div className={styles.container}>
-      <div
-        className={`${styles.sqlbar} ${isOpen ? styles.open : ""}`}
-      >
-        <div className={styles.header}></div>
-        <div className={styles.content}>{children}</div>
-      </div>
-      <button onClick={toggleMenu} className={styles.button}>
-        {isOpen ? <span>X</span> : "Open SQLBar"}
-      </button>
-    </div>
-  );
-  
-//   return (
-//     <div className={styles.container}>
-//       <div ref={side}  className={styles.sidebar} style={{ width: `${width}px`, height: '100%',  transform: `translatex(${-xPosition}px)`}}>
-//           <button onClick={() => toggleMenu()}
-//           className={styles.button} >
-//             {isOpen ? 
-//             <span>X</span> : <img src="images/avatar.png" alr="contact open button" className={styles.openBtn}/>
-//             }
-//           </button>
-//         <div className={styles.content}>{children}</div> //사이드바 컴포넌트 내부 값이 구현되는 위치
-//       </div>
-//     </div>
-//   );
+    return (
+        <div className={styles.container}>
+            <div
+                className={`${styles.sqlbar} ${isOpen ? styles.open : ""}`}
+            >
+                <div className={styles.header}></div>
+                <div className={styles.content}>{children}</div>
+            </div>
+            <button onClick={toggleMenu} className={styles.button}>
+                <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform rotate-180 ${isOpen ? "rotate-0 " : ""}`}
+                />
+            </button>
+        </div>
+    );
+   
 };
 
 export default Sqlbar;
