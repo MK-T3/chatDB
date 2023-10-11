ccefa9a821aa9bf77d3a98d0239fd34d0c567ec5
changhyeon12333 <ckd5508@naver.com>
2023-10-11 23:12:09 +0900

    merge

--- src/components/Sqlbar.jsx
+++ src/components/Sqlbar.jsx
@@ -7,14 +7,27 @@ const Sqlbar = ({ children }) => {
     const toggleMenu = () => {
         setOpen(!isOpen);
     };
-
+    const sqlQuery = `
+    CREATE TABLE Students (
+        StudentID INT PRIMARY KEY,\n
+        FirstName VARCHAR(50),\n
+        LastName VARCHAR(50),\n
+        Age INT,\n
+        GPA DECIMAL(3, 2)\n
+    );\n
+    \n
+    INSERT INTO Students (StudentID, FirstName, LastName, Age, GPA)\n
+    VALUES (1, 'John', 'Doe', 20, 3.75);\n\n
+  
+    SELECT * FROM Students;\n
+  `;
     return (
         <div className={styles.container}>
             <div
                 className={`${styles.sqlbar} ${isOpen ? styles.open : ""}`}
             >
                 <div className={styles.header}></div>
-                <div className={styles.content}>{children}</div>
+                <div className={styles.content}>{sqlQuery}</div>
             </div>
             <button onClick={toggleMenu} className={styles.button}>
                 <ChevronDownIcon
