8a5931619bfeffded7d8dff2c18439afd414f081
wogur0915 <wogur0915@gmail.com>
2023-11-07 20:54:06 +0900

    Database Diagram Save 로 문구 변경

--- src/components/Navi.jsx
+++ src/components/Navi.jsx
@@ -195,12 +195,12 @@ export default function Navi() {
                           </div>
                           <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                             <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900 mb-2">
-                              Database Schema Save
+                              Database Diagram Save
                             </Dialog.Title>
                             <hr className="border-blue-gray-50" />
                             <div className="mt-2">
                               <p className="text-sm text-gray-500">
-                                Are you going to save the schema you created?
+                                Are you going to save the Diagram you created?
                               </p>
                             </div>
                           </div>
